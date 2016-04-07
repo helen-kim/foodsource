4478a7a8fe7ec3ec1dfd9d70e03b3933a2395a7a
Annette Chen <AnnetteChen@Annettes-MBP.wv.cc.cmu.edu>
2016-04-07 18:07:33 -0400

    more

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ActiveRecord::Base
 
 	# Relationships
 	belongs_to :answer
-	has_one :question, through :answer
+	has_one :question, through: :answer
 
 	# Validations
 	validates_presence_of :answer_id, :comment
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,7 @@ class Question < ActiveRecord::Base
 
 	# Relationships
 	has_many :answers
-	has_many :comments, through :answers
+	has_many :comments, through: :answers
 
 	validates_presence_of :expertise, :ingredients 
 
